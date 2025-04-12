c9b2c2331255ce7e5e6e56ccc5b50fe7e432d265
haider <haideraltaf804@gmail.com>
2025-04-12 00:50:23 -0400

    changes

--- build.sh
+++ build.sh
@@ -11,6 +11,11 @@ python manage.py collectstatic --noinput
 echo "Applying database migrations..."
 python manage.py migrate
 
-
+if [[ $CREATE_SUPERUSER == "true" ]]; then
+    python manage.py createsuperuser --noinput \
+        --email $DJANGO_SUPERUSER_EMAIL \
+        --username $DJANGO_SUPERUSER_USERNAME
+    echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').set_password('$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
+fi
 
 echo "=== Build Complete ==="
