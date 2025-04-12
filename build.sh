9d78ceb6cc8d8aacb3c61dfa57689b9f91efa33d
haider <haideraltaf804@gmail.com>
2025-04-12 00:44:47 -0400

    login

--- build.sh
+++ build.sh
@@ -11,12 +11,6 @@ python manage.py collectstatic --noinput
 echo "Applying database migrations..."
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER == "true" ]]; then
-    echo "Creating superuser..."
-    python manage.py createsuperuser --noinput \
-        --email "${DJANGO_SUPERUSER_EMAIL:-admin@example.com}" \
-        --username "${DJANGO_SUPERUSER_USERNAME:-admin}"
-    echo "Superuser created!"
-fi
+
 
 echo "=== Build Complete ==="
